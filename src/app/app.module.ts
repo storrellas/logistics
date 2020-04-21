6e6eeb2b93a25af40a5eb4cc086569d15c71b00a
storrellas <storrellas@gmail.com>
2020-04-21 10:55:34 +0000

    ENH: Removed dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 // Imports - Project
-import { DashboardModule } from './dashboard/dashboard.module';
+
+// Imports - Other
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from "@angular/forms"; 
 
 // Providers
 
@@ -25,8 +34,18 @@ import { DashboardModule } from './dashboard/dashboard.module';
     BrowserModule,
     AppRoutingModule,
     
+
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule, 
+    FormsModule,
+
     // Project imports
-    DashboardModule
+    //DashboardModule
     
   ],
   bootstrap: [AppComponent]
