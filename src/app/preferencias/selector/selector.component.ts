6989b24cba612a5b5275645e6853d9da7cd98b81
storrellas <storrellas@gmail.com>
2020-04-22 12:02:34 +0000

    ENH: Able to select items in table

--- src/app/preferencias/selector/selector.component.ts
+++ src/app/preferencias/selector/selector.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Item {
+  code: string;
+  name: string;
+  selected: boolean;
+}
+
 @Component({
   selector: 'app-selector',
   templateUrl: './selector.component.html',
@@ -8,16 +14,16 @@ import { Component, OnInit } from '@angular/core';
 export class SelectorComponent implements OnInit {
 
   table_header: string[] = ["Código", "Nombre"]
-  table_data: object[];
-  table_selected_data: object[];
+  table_candidate_data: Item[];
+  table_selected_data: Item[];
 
   max_rows_table: number = 10;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.table_candidate_data = this.generata_random_table_data()
     this.table_selected_data = this.generata_random_table_data()
-    this.table_data = this.generata_random_table_data()
   }
 
   generate_random_str(length) {
@@ -36,16 +42,23 @@ export class SelectorComponent implements OnInit {
     const table_data = Array(this.max_rows_table).fill({})
     const random_rows : number = Math.floor(Math.random() * Math.floor(this.max_rows_table));
     for (let i = 0; i < random_rows; i++) {
-      table_data[i] = {code: this.generate_random_str(4), name: 'myname'}  
+      table_data[i] = {code: this.generate_random_str(4), 
+                        name: 'myname', selected: false}  
     }
     return table_data;
   }
 
   onSearch(event: MouseEvent){
-    this.table_data = this.generata_random_table_data()
+    this.table_candidate_data = this.generata_random_table_data()
   }
 
-  onSelectItem(event: MouseEvent, code: string){
-    console.log("Selecting element", event, code)
+  onClickCandidateItem(event: MouseEvent, code: string){
+    const index = this.table_candidate_data.findIndex( (element) => element.code == code)
+    this.table_candidate_data[index].selected = !this.table_candidate_data[index].selected; 
+  }
+
+  onClickSelectSelectedItem(event: MouseEvent, code: string){
+    const index = this.table_selected_data.findIndex( (element) => element.code == code)
+    this.table_selected_data[index].selected = !this.table_candidate_data[index].selected; 
   }
 }
