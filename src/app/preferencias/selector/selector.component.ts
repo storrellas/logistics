5b01977bdde9151cb3a6764b89a0ec3040fedaac
storrellas <storrellas@gmail.com>
2020-04-22 11:09:30 +0000

    ENH: Logic to search randomly

--- src/app/preferencias/selector/selector.component.ts
+++ src/app/preferencias/selector/selector.component.ts
@@ -9,13 +9,43 @@ export class SelectorComponent implements OnInit {
 
   table_header: string[] = ["Código", "Nombre"]
   table_data: object[];
+  table_selected_data: object[];
+
+
+  max_rows_table: number = 10;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.table_data = Array(10).fill({})
-    this.table_data[0] = {code: 'mycode', name: 'myname'}
-    this.table_data[1] = {code: 'mycode', name: 'myname'}
+    this.table_selected_data = this.generata_random_table_data()
+    this.table_data = this.generata_random_table_data()
+
+    console.log("table_selected_data", this.table_selected_data)
   }
 
+  generate_random_str(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  generata_random_table_data(){
+    
+    // Generate a max items
+    const table_data = Array(this.max_rows_table).fill({})
+    const random_rows : number = Math.floor(Math.random() * Math.floor(this.max_rows_table));
+    console.log(random_rows)
+    for (let i = 0; i < random_rows; i++) {
+      table_data[i] = {code: this.generate_random_str(4), name: 'myname'}  
+    }
+    return table_data;
+  }
+
+  onSearch(event: MouseEvent){
+    this.table_data = this.generata_random_table_data()
+  }
 }
