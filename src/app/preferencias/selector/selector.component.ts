e4ef44f94bdff10500585ad83a152d564403d8e2
storrellas <storrellas@gmail.com>
2020-04-23 08:59:35 +0000

    ENH: Unable to select empty rows

--- src/app/preferencias/selector/selector.component.ts
+++ src/app/preferencias/selector/selector.component.ts
@@ -53,10 +53,12 @@ export class SelectorComponent implements OnInit {
 
   // Selectable items
   onClickCandidateItem(event: MouseEvent, code: string){
+    if( code == undefined ) return;
     const index = this.table_candidate_data.findIndex( (element) => element.code == code)
     this.table_candidate_data[index].selected = !this.table_candidate_data[index].selected; 
   }
   onClickSelectSelectedItem(event: MouseEvent, code: string){
+    if( code == undefined ) return;
     const index = this.table_selected_data.findIndex( (element) => element.code == code)
     this.table_selected_data[index].selected = !this.table_candidate_data[index].selected; 
   }
