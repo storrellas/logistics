d76f08c5cabebdda6e011dd71308a74e033e702f
storrellas <storrellas@gmail.com>
2020-04-28 22:00:59 +0000

    ENH: Adding modal bootstrap with animation

--- src/app/portic-common/document/document.component.ts
+++ src/app/portic-common/document/document.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+
 @Component({
   selector: 'document',
   templateUrl: './document.component.html',
@@ -9,6 +12,8 @@ export class DocumentComponent implements OnInit {
 
   port_id: string = "port_load";
   port_name: string = "Puerto de Carga";
+  closeResult: string = '';
+
 
   @Input()
   set type(value: string) {
@@ -24,9 +29,28 @@ export class DocumentComponent implements OnInit {
     }
   }
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
 
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
 }
+
