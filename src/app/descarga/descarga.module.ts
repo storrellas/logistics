1d4d81a3d65fe58aa23d5382734126097d99df52
storrellas <storrellas@gmail.com>
2020-04-28 21:24:56 +0000

    ENH: Adding descarga

--- src/app/descarga/descarga.module.ts
+++ src/app/descarga/descarga.module.ts
@@ -4,12 +4,16 @@ import { DescargaComponent } from './descarga.component';
 
 // Import
 import { PorticCommonModule } from '../portic-common/portic-common.module'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [DescargaComponent],
   imports: [
     CommonModule,
-    PorticCommonModule 
+    NgbModule,
+    PorticCommonModule,
+    FontAwesomeModule
   ]
 })
 export class DescargaModule { }
